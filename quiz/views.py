6e11da42eaf3f72352ef004f81d56fa831b75068
Shameem <shameemulhassan87@gmail.com>
2026-05-06 11:23:37 +0530

    Implemented single question quiz interface

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
+
+def quiz_view(request):
+    questions = Question.objects.all()
+
+    context = {
+        'questions': questions
+    }
+
+    return render(request, 'quiz/quiz.html', context)
