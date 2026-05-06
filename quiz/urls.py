babd88a490f7fc2e84e69f3af086ea3ee5d6814c
Shameem <shameemulhassan87@gmail.com>
2026-05-06 11:36:35 +0530

    Implemented session based quiz flow

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.quiz_view, name='quiz'),
+ path('', views.start_quiz, name='start_quiz'),
+    path('question/<int:index>/', views.question_view, name='question'),
+    path('result/', views.result_view, name='result'),
+    
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,13 +1,56 @@
-from django.shortcuts import render
-from .models import Question
+from django.shortcuts import render, redirect
+from .models import Question, Choice
 
-# Create your views here.
 
-def quiz_view(request):
-    questions = Question.objects.all()
+def start_quiz(request):
+
+    request.session['score'] = 0
+
+    return redirect('question', index=0)
+
+
+def question_view(request, index):
+
+    questions = list(Question.objects.all())
+
+    total_questions = len(questions)
+
+    if index >= total_questions:
+        return redirect('result')
+
+    question = questions[index]
+
+    if request.method == 'POST':
+
+        selected_choice = request.POST.get('choice')
+
+        if selected_choice:
+
+            choice = Choice.objects.get(id=selected_choice)
+
+            if choice.is_correct:
+                request.session['score'] += 1
+
+        return redirect('question', index=index + 1)
 
     context = {
-        'questions': questions
+        'question': question,
+        'index': index + 1,
+        'total': total_questions
     }
 
-    return render(request, 'quiz/quiz.html', context)
+    return render(request, 'quiz/question.html', context)
+
+
+def result_view(request):
+
+    score = request.session.get('score', 0)
+
+    total_questions = Question.objects.count()
+
+    context = {
+        'score': score,
+        'total': total_questions
+    }
+
+    return render(request, 'quiz/result.html', context)
