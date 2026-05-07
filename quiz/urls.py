b76b04028b081c391b501ef33c8e27c46408ebd4
Shameem <shameemulhassan87@gmail.com>
2026-05-07 11:17:41 +0530

    Added homepage and answer review system

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
- path('', views.start_quiz, name='start_quiz'),
+    path('', views.home_view, name='home'),
+    path('start/', views.start_quiz, name='start_quiz'),
     path('question/<int:index>/', views.question_view, name='question'),
     path('result/', views.result_view, name='result'),
-    
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render, redirect
 from .models import Question, Choice
 
 
+def home_view(request):
+    return render(request, 'quiz/home.html')
+
+
 def start_quiz(request):
 
     request.session['score'] = 0
 
-    # Randomize questions ONCE
+    request.session['answers'] = {}
+
     question_ids = list(
         Question.objects.order_by('?').values_list('id', flat=True)
     )
@@ -22,7 +27,6 @@ def question_view(request, index):
 
     total_questions = len(question_ids)
 
-    # Quiz finished
     if index >= total_questions:
         return redirect('result')
 
@@ -34,12 +38,18 @@ def question_view(request, index):
 
         selected_choice = request.POST.get('choice')
 
+        answers = request.session.get('answers', {})
+
         if selected_choice:
+
+            answers[str(question.id)] = int(selected_choice)
 
             choice = Choice.objects.get(id=selected_choice)
 
             if choice.is_correct:
                 request.session['score'] += 1
+
+        request.session['answers'] = answers
 
         return redirect('question', index=index + 1)
 
@@ -56,13 +66,33 @@ def result_view(request):
 
     score = request.session.get('score', 0)
 
-    total_questions = len(
-        request.session.get('question_ids', [])
-    )
+    question_ids = request.session.get('question_ids', [])
+
+    answers = request.session.get('answers', {})
+
+    review_data = []
+
+    for question_id in question_ids:
+
+        question = Question.objects.get(id=question_id)
+
+        choices = question.choice_set.all()
+
+        selected_choice_id = answers.get(str(question.id))
+
+        correct_choice = choices.get(is_correct=True)
+
+        review_data.append({
+            'question': question,
+            'choices': choices,
+            'selected_choice_id': selected_choice_id,
+            'correct_choice_id': correct_choice.id
+        })
 
     context = {
         'score': score,
-        'total': total_questions
+        'total': len(question_ids),
+        'review_data': review_data
     }
 
     return render(request, 'quiz/result.html', context)
